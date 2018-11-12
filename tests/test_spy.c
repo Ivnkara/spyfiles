d04915417bf7b204ac98236f29b6b39261b72349
rogovykh <rogovykh@kolesa.kz>
2018-11-12 08:39:38 +0600

    Try use pipe and dup

--- tests/test_spy.c
+++ tests/test_spy.c
@@ -30,6 +30,15 @@ void * wr_file()
 
 static char * test_start_spy()
 {
+	char expected_stdout[] = "Path: \t\t\tName: \tSize:\n";
+	int old_stdout = dup(1);
+	int p[2];
+	pipe(p);
+	dup2(p[1], 1);
+
+	char buf[1024];
+	char *ptr = buf;
+
 	pthread_t wr_file_thread;
 	pthread_create(&wr_file_thread, NULL, wr_file, NULL);
 
@@ -37,15 +46,33 @@ static char * test_start_spy()
 		result   = start_spy(PATH_DIR, 0),
 		expected = 0;
 
+	do {
+		read(p[0], ptr, 1);
+		ptr++;
+	} while(ptr[-1] != '\n');
+	fflush(stdout);
 
 	mu_assert("---> ERROR, start_spy return not zero", expected == result);
+	mu_assert("---> ERROR, stdout not equals expected", strcmp(expected_stdout, buf) == 0);
 	pthread_join(wr_file_thread, NULL);
+	*ptr = 0;
+
+	dup2(old_stdout, 1);
 
 	return 0;
 }
 
 static char * test_spy_dir()
 {
+	char expected_stdout[] = "Path: \t\t\tName: \tSize:\n";
+	int old_stdout = dup(1);
+	int p[2];
+	pipe(p);
+	dup2(p[1], 1);
+
+	char buf[1024];
+	char *ptr = buf;
+
 	pthread_t wr_file_thread;
 	pthread_create(&wr_file_thread, NULL, wr_file, NULL);
 
@@ -55,11 +82,21 @@ static char * test_spy_dir()
 	wd     = inotify_add_watch(fd, PATH_DIR, IN_MODIFY | IN_CREATE | IN_DELETE);
 	result = spy_dir(fd);
 
+	do {
+		read(p[0], ptr, 1);
+		ptr++;
+	} while(ptr[-1] != '\n');
+	fflush(stdout);
+
 	mu_assert("---> ERROR, spy_dir return not zero", expected == result);
+	mu_assert("---> ERROR, stdout not equals expected", strcmp(expected_stdout, buf) == 0);
 
 	inotify_rm_watch(fd, wd);
 	close(fd);
 	pthread_join(wr_file_thread, NULL);
+	*ptr = 0;
+
+	dup2(old_stdout, 1);
 
 	return 0;
 }
