3e9fb8fe9d5114d2ace9ba34bcd1b88474199704
Ivan <irogovykh@gmail.com>
2018-11-25 17:33:12 +0600

    Ended tests

--- tests/test_spy.c
+++ tests/test_spy.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/inotify.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #include "../src/spy.h"
@@ -16,18 +17,21 @@ char * tempfile_for_event;
 char * tempdir_for_filesize;
 char * tempfile_for_filesize;
 
+char * tempdir_for_print;
+char * tempfile_for_print;
+
 int inotify_fd;
 
 void * wr_file(void * which_file)
 {
-	sleep(1);
 	int fd;
 	int type = *(int *)which_file;
+
 
 	if (type == 1) {
 		fd = open(tempfile_for_event, O_CREAT | O_WRONLY | O_TRUNC, 0777);
 	} else if (type == 2) {
-		fd = open(tempfile_for_filesize, O_CREAT | O_WRONLY | O_TRUNC, 0777);
+		fd = open(tempfile_for_filesize, O_CREAT | O_WRONLY | O_APPEND, 0777);
 	}
 
 	if (fd < 0) {
@@ -35,7 +39,7 @@ void * wr_file(void * which_file)
 	}
 
 	char test_line[] = "data for spy tests";
-	write(fd, test_line, 32);
+	write(fd, test_line, 20);
 	close(fd);
 
 	*(int *)which_file = type;
@@ -86,35 +90,38 @@ static char * test_scan_dir()
 
 static char * test_check_filesize()
 {
-	char expected[] = "-----> Размер файла tmp.txt увеличился на 19 байт\n";
+	char expected[] = "-----> Размер файла tmp.txt увеличился на 20 байт";
 	int old_stdout = dup(1);
 	int p[2];
 	pipe(p);
 	dup2(p[1], 1);
-
-	char buf[1024];
+	char buf[80];
 	char *ptr = buf;
-
 	int arg = 2;
-	pthread_t wr_file_thread;
-	pthread_create(&wr_file_thread, NULL, wr_file, &arg);
+	
+	pthread_t wr_file_thread1, wr_file_thread2;
+	pthread_create(&wr_file_thread1, NULL, wr_file, &arg);
 
-	int result = check_filesize(tempdir_for_filesize);
+	void * ret1, * ret2;
+	pthread_join(wr_file_thread1, &ret1);
+
+	scan_dir(tempdir_for_filesize, 0);
+
+	pthread_create(&wr_file_thread2, NULL, wr_file, &arg);
+	pthread_join(wr_file_thread2, &ret2);
+
+	int result = check_filesize("tmp.txt");
 
 	do {
 		read(p[0], ptr, 1);
 		ptr++;
 	} while(ptr[-1] != '\n');
+	ptr[-1] = '\0';
 
 	fflush(stdout);
 
-	printf("%s\n", buf);
-
 	mu_assert("---> ERROR, start_spy return not zero", 0 == result);
 	mu_assert("---> ERROR, stdout not equals expected", strcmp(expected, buf) == 0);
-
-	void *ret;
-	pthread_join(wr_file_thread, &ret);
 	*ptr = 0;
 
 	dup2(old_stdout, 1);
@@ -125,6 +132,90 @@ static char * test_check_filesize()
 	return 0;
 }
 
+static char * test_print_changes_file()
+{
+	char expected[] = "-----> Новые данные в файле:";
+	int old_stdout = dup(1);
+	int p[2];
+	pipe(p);
+	dup2(p[1], 1);
+	char buf[80];
+	char *ptr = buf;
+	int arg = 2;
+	
+	pthread_t wr_file_thread1, wr_file_thread2;
+	pthread_create(&wr_file_thread1, NULL, wr_file, &arg);
+
+	void * ret1, * ret2;
+	pthread_join(wr_file_thread1, &ret1);
+
+	scan_dir(tempdir_for_filesize, 0);
+
+	pthread_create(&wr_file_thread2, NULL, wr_file, &arg);
+	pthread_join(wr_file_thread2, &ret2);
+
+	print_changes_file(tempfile_for_filesize, 20);
+
+	do {
+		read(p[0], ptr, 1);
+		ptr++;
+	} while(ptr[-1] != '\n');
+	ptr[-1] = '\0';
+
+	fflush(stdout);
+
+	mu_assert("---> ERROR, stdout not equals expected", strcmp(expected, buf) == 0);
+	*ptr = 0;
+
+	dup2(old_stdout, 1);
+	close(old_stdout);
+	close(p[0]);
+	close(p[1]);
+
+	return 0;
+}
+
+void initTmp(char * tmp1, char * tmp2)
+{
+	tempfile_for_event = calloc(64, sizeof(char));
+	tempdir_for_event = calloc(64, sizeof(char));
+	tempfile_for_filesize = calloc(64, sizeof(char));
+	tempdir_for_filesize = calloc(64, sizeof(char));
+
+	tempdir_for_event = mkdtemp(tmp1);
+	tempdir_for_filesize = mkdtemp(tmp2);
+
+	strcat(tempdir_for_event, "/");
+	strcat(tempdir_for_filesize, "/");
+	
+	strcpy(tempfile_for_event, tempdir_for_event);
+	strcat(tempfile_for_event, "tmp.txt");
+	strcpy(tempfile_for_filesize, tempdir_for_filesize);
+	strcat(tempfile_for_filesize, "tmp.txt");
+
+	printf("Create temp directory for test: %s\n", tempdir_for_event);
+	printf("Create path temp file for test: %s\n", tempfile_for_event);
+	printf("Create temp directory for test: %s\n", tempdir_for_filesize);
+	printf("Create path temp file for test: %s\n", tempfile_for_filesize);
+}
+
+void removeTmp()
+{
+	int res1 = unlink(tempfile_for_event);
+	int res2 = unlink(tempfile_for_filesize);
+
+	if (res1 < 0 || res2 < 0) {
+		perror("Unlink fail: ");
+	}
+
+	int res3 = rmdir(tempdir_for_event);
+	int res4 = rmdir(tempdir_for_filesize);
+
+	if (res3 < 0 || res4 < 0) {
+		perror("Rmdir fail: ");
+	}
+}
+
 int tests_run = 0;
 
 static char * all_tests()
@@ -133,6 +224,7 @@ static char * all_tests()
 	mu_run_test(test_get_watch_wd);
 	mu_run_test(test_get_event);
 	mu_run_test(test_scan_dir);
+	mu_run_test(test_print_changes_file);
 	mu_run_test(test_check_filesize);
 
 	return 0;
@@ -140,25 +232,9 @@ static char * all_tests()
 
 int main()
 {
-	char tmp[] = "/tmp/spytest-XXXXXX";
-
-	tempfile_for_event = calloc(64, sizeof(char));
-	tempdir_for_event = calloc(64, sizeof(char));
-	tempfile_for_filesize = calloc(64, sizeof(char));
-	tempdir_for_filesize = calloc(64, sizeof(char));
-	
-	tempdir_for_event = mkdtemp(tmp);
-	tempdir_for_filesize = mkdtemp(tmp);
-	
-	strcpy(tempfile_for_event, tempdir_for_event);
-	strcat(tempfile_for_event, "/tmp.txt");
-	strcpy(tempfile_for_filesize, tempdir_for_filesize);
-	strcat(tempfile_for_filesize, "/tmp.txt");
-
-	printf("Create temp directory for test: %s\n", tempdir_for_event);
-	printf("Create path temp file for test: %s\n", tempfile_for_event);
-	printf("Create temp directory for test: %s\n", tempdir_for_filesize);
-	printf("Create path temp file for test: %s\n", tempfile_for_filesize);
+	char tmp1[] = "/tmp/spytest1-XXXXXX";
+	char tmp2[] = "/tmp/spytest2-XXXXXX";
+	initTmp(tmp1, tmp2);
 
 	char * result = all_tests();
 
@@ -169,6 +245,8 @@ int main()
 	}
 
 	printf("Tests run: %d\n", tests_run);
+
+	removeTmp();
 
 	return 0;
 }
