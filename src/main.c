8bdc8089d19d5d1bdcd68e71c3e9bc4b567f963e
Ivan <irogovykh@gmail.com>
2019-01-12 15:41:06 +0600

    Small refactor main.c

--- src/main.c
+++ src/main.c
@@ -9,14 +9,12 @@
 int main(int argc, char const *argv[])
 {
 	if (argc != 2) {
-		fprintf(stderr, "Use spy-files /path/to/spy/dir\n");
+		fprintf(stderr, "Use spy-files /path/to/spy/dir1 [/path/to/spy/dir2 ...]\n");
 
 		return 1;
 	}
 
-	int inotify_fd, scan_dir_result, add_watch_result;
-
-	inotify_fd = get_inotify_fd();
+	int inotify_fd = get_inotify_fd();
 
 	if (inotify_fd < 0) {
 		fprintf(stderr, "Error get inotify fd\n");
@@ -24,17 +22,13 @@ int main(int argc, char const *argv[])
 		return 1;
 	}
 
-	scan_dir_result = scan_dir((char *)argv[1], 1, 1);
-
-	if (scan_dir_result < 0) {
+	if (scan_dir((char *)argv[1], 1, 1) < 0) {
 		fprintf(stderr, "Error scan dir\n");
 
 		return 1;
 	}
 
-	add_watch_result = get_watch_wd((char *)argv[1], inotify_fd);
-
-	if (add_watch_result < 0) {
+	if (get_watch_wd((char *)argv[1], inotify_fd) < 0) {
 		fprintf(stderr, "Error get watch wd\n");
 
 		return 1;
@@ -45,10 +39,12 @@ int main(int argc, char const *argv[])
 
 	while (1) {
 		scan_dir((char *)argv[1], 0, 0);
+	
 		count = get_event(inotify_fd, events);
+	
 		prepare_event(count, events);
-		bzero(events, EVENT_SIZE);
+		bzero(events, BUF_LEN);
 	}
 
-	return 0;
+	exit(EXIT_SUCCESS);
 }
